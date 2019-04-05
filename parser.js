b755cf6260dc2eb1db9e38409fa47d8ef3edcaa4
Felipe Polo-Wood <fpolowood@users.noreply.github.com>
2019-04-05 06:36:25 -0400

    Fixes #1, switch splitting from string to regex

--- parser.js
+++ parser.js
@@ -4,13 +4,8 @@ module.exports = {
   parse: (manifestYaml) => {
     console.log('Parsing manifests...')
     
-    let manifests = manifestYaml.split('---').map(y => YAML.parse(y))
-
-    // First element is null due to leading delimiter in file.
-    if (manifests.length > 0 && !manifests[0]) {
-      manifests = manifests.slice(1)
-    }
-
+    let manifests = manifestYaml.split(/^---$/gm).map(y => YAML.parse(y)).filter(y=>y)
+    
     return manifests
   }
 }
